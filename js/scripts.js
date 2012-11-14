a272c31a3377c77f42b65605656a5fe061d80703
Elliott Regan <regan.elliott@gmail.com>
2012-11-14 17:53:27 -0500

    Added a bunch of pages. Need specific styling.

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,23 @@
 $(document).ready( function() {
 
-  $.plot($('#progress_graph'), [[[1, 2],[3,3],[5,7],[7,5],[9,5],[11,4]]]);
+  data = [[[1, 2],[3,3],[5,7],[7,5],[9,5],[11,4]]];
+ 
+  $.plot($('#pie_chart'), data); 
+  
+ 
+  var pie.data = [];
+  var series = Math.floor(Math.random()*6)+3;
+  for( var i = 0; i<series; i++){false
+  	data[i] = { label: "Series"+(i+1), data: Math.floor(Math.random()*100)+1 }
+  }
+
+  $.plot($('#pie_chart'), pie.data, 
+  {
+    series: {
+      pie: {
+        show: true
+      }
+    }
+  });
 
 });
