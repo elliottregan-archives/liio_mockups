7aaf029ad229a372c54c158223817dabca51e5e4
Elliott Regan <regan.elliott@gmail.com>
2012-11-15 01:07:33 -0500

    Many UI Improvements to the Dashboard and Goal_home

--- js/scripts.js
+++ js/scripts.js
@@ -2,22 +2,22 @@ $(document).ready( function() {
 
   data = [[[1, 2],[3,3],[5,7],[7,5],[9,5],[11,4]]];
  
-  $.plot($('#pie_chart'), data); 
+  $.plot($('#progress_graph'), data); 
   
  
-  var pie.data = [];
-  var series = Math.floor(Math.random()*6)+3;
-  for( var i = 0; i<series; i++){false
-  	data[i] = { label: "Series"+(i+1), data: Math.floor(Math.random()*100)+1 }
-  }
-
-  $.plot($('#pie_chart'), pie.data, 
-  {
-    series: {
-      pie: {
-        show: true
-      }
-    }
-  });
+//  var pieData = [];
+//  var series = Math.floor(Math.random()*6)+3;
+//  for( var i = 0; i<series; i++){false
+//  	data[i] = { label: "Series"+(i+1), data: Math.floor(Math.random()*100)+1 }
+//  }
+//
+//  $.plot($('#pie_chart'), pieData, 
+//  {
+//    series: {
+//      pie: {
+//        show: true
+//      }
+//    }
+//  });
 
 });
