d3cfb568e505c88311768fb5b35661a9624d8854
Elliott Regan <regan.elliott@gmail.com>
2012-11-30 11:06:14 -0800

    More Responsive Dashboard

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,20 @@
 $(document).ready( function() {
 
   data = [[[1, 2],[3,3],[5,7],[7,5],[9,5],[11,4]]];
+  
+  options = {
+    series: {
+      lines: {show: true},
+      
+      points: { show:true},
+      
+      hoverable: true
+    }
+    
+
+  }
  
-  $.plot($('#progress_graph'), data); 
+  $.plot($('#progress_graph'), data, options); 
   
  
 //  var pieData = [];
