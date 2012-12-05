466e047e056881fbef2931afa777f05ad8a230e3
Elliott Regan <regan.elliott@gmail.com>
2012-12-05 01:38:01 -0500

    Added a "new inspiration" button

--- js/scripts.js
+++ js/scripts.js
@@ -36,12 +36,30 @@ $(document).ready( function() {
   $('#cal, #col').change(function() {
     if ($('#cal')[0].checked == true) {
       $('#stats').fadeOut();
-      $('#photo_grid').delay(400).fadeIn();
+      $('#photo_grid, button.new').delay(400).fadeIn();
     }
     else {
       $('#stats').delay(400).fadeIn();
-      $('#photo_grid').fadeOut();
+      $('#photo_grid, button.new').fadeOut();
     }
   });
+  
+  $('.edit').click( function() {
+    $(this).fadeOut();
+    $(this).next('form').slideDown();
+  })
+  
+  $('.cancel').click( function() {
+    $(this).parent().parent().fadeOut()
+    $(this).parent().parent().siblings('.edit').fadeIn()
+  })
+  
+  $('button.new').click( function() {
+    $('li.new_insp').slideDown()
+  })
+  
+  $('button.close').click( function() {
+    $('li.new_insp').slideUp()
+  })
 
 });
