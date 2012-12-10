756afd921bdc19916405dbcc6a8f5905dea64df7
Elliott Regan <regan.elliott@gmail.com>
2012-12-10 17:07:28 -0500

    Cleaned up the Plan Page. New JS file just for graph stuff.

---
+++ js/graph_scripts.js
@@ -0,0 +1,34 @@
+$(document).ready( function() {
+
+  data = [[[1, 2],[3,3],[5,7],[7,5],[9,5],[11,4]]];
+  
+  options = {
+    series: {
+      lines: {show: true},
+      
+      points: { show:true},
+      
+      hoverable: true
+    }
+    
+
+  }
+ 
+  $.plot($('#progress_graph'), data, options); 
+  
+  
+  //  var pieData = [];
+  //  var series = Math.floor(Math.random()*6)+3;
+  //  for( var i = 0; i<series; i++){false
+  //  	data[i] = { label: "Series"+(i+1), data: Math.floor(Math.random()*100)+1 }
+  //  }
+  //
+  //  $.plot($('#pie_chart'), pieData, 
+  //  {
+  //    series: {
+  //      pie: {
+  //        show: true
+  //      }
+  //    }
+  //  });
+})
--- js/scripts.js
+++ js/scripts.js
@@ -1,37 +1,4 @@
-$(document).ready( function() {
-
-  data = [[[1, 2],[3,3],[5,7],[7,5],[9,5],[11,4]]];
-  
-  options = {
-    series: {
-      lines: {show: true},
-      
-      points: { show:true},
-      
-      hoverable: true
-    }
-    
-
-  }
- 
-  $.plot($('#progress_graph'), data, options); 
-  
- 
-//  var pieData = [];
-//  var series = Math.floor(Math.random()*6)+3;
-//  for( var i = 0; i<series; i++){false
-//  	data[i] = { label: "Series"+(i+1), data: Math.floor(Math.random()*100)+1 }
-//  }
-//
-//  $.plot($('#pie_chart'), pieData, 
-//  {
-//    series: {
-//      pie: {
-//        show: true
-//      }
-//    }
-//  });
-
+$(document).ready( function() {  
 
   $('#cal, #col').change(function() {
     if ($('#cal')[0].checked == true) {
@@ -60,6 +27,10 @@ $(document).ready( function() {
   
   $('button.close').click( function() {
     $('li.new_insp').slideUp()
+  })
+  
+  $('#focus_areas li li').click( function() {
+    $(this).toggleClass('selected')
   })
 
 });
